b04fd5af63542c8ff55f50c17493499003d96508
Kenneth Hoste <kenneth.hoste@ugent.be>
2021-09-07 15:08:19 +0200

    update for boegelbot test reports on new generoso cluster

--- eb_from_pr_upload_generoso.sh
+++ eb_from_pr_upload_generoso.sh
@@ -6,21 +6,30 @@
 
 set -e
 
-module use /shared/easybuilder/CentOS8/haswell/modules/all
+TOPDIR="/project"
+
+module use $TOPDIR/$USER/CentOS8/haswell/modules/all
 
 EB_PREFIX=$HOME/easybuild
 export PYTHONPATH=$EB_PREFIX/easybuild-framework:$EB_PREFIX/easybuild-easyblocks:$EB_PREFIX/easybuild-easyconfigs
 # $HOME/.local/bin is added to $PATH for Python packages like archspec installed with 'pip install --user'
 export PATH=$EB_PREFIX/easybuild-framework:$HOME/.local/bin:$PATH
 
-export EASYBUILD_PREFIX=$HOME/CentOS8/$(archspec cpu)
+# hardcode to haswell for now, workernodes are actually a mix of haswell/broadwell (but seems to work fine)
+export EASYBUILD_PREFIX=$TOPDIR/$USER/Rocky8/haswell
 export EASYBUILD_BUILDPATH=/tmp/$USER
-export EASYBUILD_SOURCEPATH=$EASYBUILD_PREFIX/sources:/shared/maintainers/sources
+export EASYBUILD_SOURCEPATH=$TOPDIR/$USER/sources:$TOPDIR/maintainers/sources
 
 export EASYBUILD_GITHUB_USER=boegelbot
 
 export EB_PYTHON=python3
 
 export EASYBUILD_ACCEPT_EULA_FOR='.*'
+
+export EASYBUILD_HOOKS=$HOME/boegelbot/eb_hooks.py
+
+export EASYBUILD_CUDA_COMPUTE_CAPABILITIES=7.0
+
+export INTEL_LICENSE_FILE=$TOPDIR/maintainers/licenses/intel.lic
 
 eb --from-pr $EB_PR --debug --rebuild --robot --upload-test-report --download-timeout=1000 $EB_ARGS
