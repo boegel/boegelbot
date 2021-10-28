380b08449445f0fc7c022379f838d332b75722c7
Kenneth Hoste <kenneth.hoste@ugent.be>
2021-10-28 14:27:37 +0200

    fix 'module use' statement in script to test EasyBuild PRs

--- eb_from_pr_upload_generoso.sh
+++ eb_from_pr_upload_generoso.sh
@@ -8,8 +8,6 @@ set -e
 
 TOPDIR="/project"
 
-module use $TOPDIR/$USER/CentOS8/haswell/modules/all
-
 EB_PREFIX=$HOME/easybuild
 export PYTHONPATH=$EB_PREFIX/easybuild-framework:$EB_PREFIX/easybuild-easyblocks:$EB_PREFIX/easybuild-easyconfigs
 # $HOME/.local/bin is added to $PATH for Python packages like archspec installed with 'pip install --user'
@@ -31,5 +29,7 @@ export EASYBUILD_HOOKS=$HOME/boegelbot/eb_hooks.py
 export EASYBUILD_CUDA_COMPUTE_CAPABILITIES=7.0
 
 export INTEL_LICENSE_FILE=$TOPDIR/maintainers/licenses/intel.lic
+
+module use $EASYBUILD_PREFIX/modules/all
 
 eb --from-pr $EB_PR --debug --rebuild --robot --upload-test-report --download-timeout=1000 $EB_ARGS
