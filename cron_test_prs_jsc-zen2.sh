dc4595c6959e2208e46cfe3592f3f1516d6a2dae
Sebastian Achilles <s.achilles@fz-juelich.de>
2022-01-15 17:19:59 +0100

    Add files for jsc-zen2

---
+++ cron_test_prs_jsc-zen2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# change to directory in which this script is located
+cd `dirname $(realpath $0)`
+
+# update EasyBuild to latest 'develop' branches
+./easybuild_develop.sh
+
+# set up environment
+EB_PREFIX=$HOME/easybuild source init_env_easybuild_develop.sh
+
+# check notifications for new PRs to test
+./test_prs_jsc-zen2.sh
---
+++ eb_from_pr_upload_jsc-zen2.sh
@@ -0,0 +1,39 @@
+#!/bin/bash -l
+#SBATCH --nodes 1
+#SBATCH --ntasks=4
+#SBATCH --time 100:0:0
+#SBATCH --output /project/def-maintainers/boegelbot/slurmjobs/slurm-%j.out
+#SBATCH --get-user-env
+
+set -e
+
+TOPDIR="/project/def-maintainers"
+
+module use $TOPDIR/$USER/Rocky8/zen2/modules/all
+
+EB_PREFIX=$HOME/easybuild
+export PYTHONPATH=$EB_PREFIX/easybuild-framework:$EB_PREFIX/easybuild-easyblocks:$EB_PREFIX/easybuild-easyconfigs
+# $HOME/.local/bin is added to $PATH for Python packages like archspec installed with 'pip install --user'
+export PATH=$EB_PREFIX/easybuild-framework:$HOME/.local/bin:$PATH
+
+export EASYBUILD_PREFIX=$TOPDIR/$USER/Rocky8/zen2
+export EASYBUILD_BUILDPATH=/tmp/$USER
+export EASYBUILD_SOURCEPATH=$TOPDIR/$USER/sources
+
+export EASYBUILD_GITHUB_USER=boegelbot
+
+export EB_PYTHON=python3
+
+export EASYBUILD_ACCEPT_EULA_FOR='.*'
+
+export EASYBUILD_HOOKS=$HOME/boegelbot/eb_hooks.py
+
+export EASYBUILD_CUDA_COMPUTE_CAPABILITIES=7.0
+
+export EASYBUILD_SET_GID_BIT=1
+
+export EASYBUILD_UMASK='022'
+
+module use $EASYBUILD_PREFIX/modules/all
+
+eb --from-pr $EB_PR --debug --rebuild --robot --upload-test-report --download-timeout=1000 $EB_ARGS
