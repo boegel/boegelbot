f92b3bd01ed6f1fe4cb980bac9952c775b97a00b
Kenneth Hoste <kenneth.hoste@ugent.be>
2026-06-10 14:24:28 +0200

    use $BASH_SOURCE instead of $ to determine path to init_env_easybuild_develop.sh script relative to easybuild_develop.sh

--- easybuild_develop.sh
+++ easybuild_develop.sh
@@ -15,7 +15,7 @@ else
 fi
 mkdir -p $EB_PREFIX
 
-INIT_ENV=`dirname $(realpath $0)`/init_env_easybuild_develop.sh
+INIT_ENV=`dirname ${BASH_SOURCE}`/init_env_easybuild_develop.sh
 if [ ! -f $INIT_ENV ]; then
     echo "$INIT_ENV not found!?" >&2
     exit 1
